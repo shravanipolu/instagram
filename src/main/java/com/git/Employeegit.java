d2f33cf04cab310a5ca2001ac156ddff589d371f
Shravani <Shravani@Shravani>
2019-06-11 14:08:19 +0530

    dionr

--- src/main/java/com/git/Employeegit.java
+++ src/main/java/com/git/Employeegit.java
@@ -1,5 +1,21 @@
 package com.git;
 
+
 public class Employeegit {
+private String name;
+private String mailId;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getMailId() {
+	return mailId;
+}
+public void setMailId(String mailId) {
+	this.mailId = mailId;
+}
+
 
 }
